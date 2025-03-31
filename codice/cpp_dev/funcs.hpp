a096abb2f46da5f6c8d73b770a5490b188d30f2e
Matteo Leone <matteo4.leone@mail.polimi.it>
2025-03-31 13:58:32 +0000

    Boh non ci sto capendo nulla ci sono 2000 gradi e non riesco a pensare

--- codice/cpp_dev/funcs.hpp
+++ codice/cpp_dev/funcs.hpp
@@ -12,15 +12,17 @@
 template<typename T>
 T u_exact (const std::vector<T> &x,const T &t)
 {
-    return std::sin(x[0])*std::sin(x[1])*std::sin(t);
-    //return ( x[0]*(x[0] - 2*M_PI) + x[1]*(x[1]- 2*M_PI) )*t*t*t;
+    return std::sin(t);
+    // return std::sin(x[0])*std::sin(x[1])*std::sin(t);
+    // return ( x[0]*(x[0] - 2*M_PI) + x[1]*(x[1]- 2*M_PI) )*t*t*t;
 } 
 
 // Forcing term definition
 template<typename T>
 T f(const std::vector<T> &x,const T &t)
 {
-    return (std::cos(t) - 2)*std::sin(x[0])*std::sin(x[1]);
+    return -std::cos(t);
+    // return (std::cos(t) - 2)*std::sin(x[0])*std::sin(x[1]);
     // return  ( x[0]*(x[0] - 2*M_PI) + x[1]*(x[1]- 2*M_PI) )*3*t*t;
 }
 
